a1652225b2735bf6b2d2c50945ab3e811f19fadc
JanaRobbins <128938375+JanaRobbins@users.noreply.github.com>
2023-05-28 10:49:58 +0100

    import dependencies to map_vector_data
    dependencies are same like for practical 2 - mapping with cartopy

---
+++ map_with_cartopy.py
@@ -0,0 +1,7 @@
+import os
+import geopandas as gpd
+import matplotlib.pyplot as plt
+from cartopy.featrue import ShapelyFeature
+import cartopy.crs as ccrs
+import matplotlip.patches as mpatches
+import matplotlib.lines as mlines
