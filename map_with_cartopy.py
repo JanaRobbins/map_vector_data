b5b2c88af194e9f89bb651d5ce1147612adc1311
JanaRobbins <128938375+JanaRobbins@users.noreply.github.com>
2023-05-30 18:31:58 +0100

    generate matplotlib handles

--- map_with_cartopy.py
+++ map_with_cartopy.py
@@ -5,3 +5,10 @@ from cartopy.featrue import ShapelyFeature
 import cartopy.crs as ccrs
 import matplotlip.patches as mpatches
 import matplotlib.lines as mlines
+
+def generate_handles(labels, color, edge='k', alpha=1):
+    lc=len(colors)
+    handles=[]
+    for i in range(len(lables)):
+        handles.append(mpatches.Rectangle((0,0),1,1,facecolor=colors[i % lc], edgecolor=edge, alpha=alpha))
+        return handles
